72d4cb2f2568508ced0f505bd86a38dc50ca9c2b
Aaron Allsup <91850436+Meanravage@users.noreply.github.com>
2022-02-04 19:40:34 -0700

    completed file 02/04/2022
    Finalized program for homographs, just needs notation.

--- homographweek05/Main.cpp
+++ homographweek05/Main.cpp
@@ -1,75 +1,99 @@
-#include <cstring>
 #include <iostream>
-#include <stack>
+#include <cstring>
+#include <filesystem>
 #include <vector>
 #include <sstream>
-#include <filesystem>
+#include <map>
+#include <stdlib.h>
 
 using namespace std;
 using namespace std::filesystem;
 
-bool homograph(string file1, string file2);
-string canon(vector<string> filePath);
-stack<string> reverseStack(stack<string> stringStack);
-vector<string> stringToVector(string file);
-string getCurrentPath(stack<string>& pathVector);
+string prompt1(string file1);
+string prompt2(string file2);
+vector<string> stringConverter(string fileName);
+string canonicalizer(vector<string> filePath);
+bool homograph(string filePath1, string filePath2);
+void allPathsCheck(string filePath1, string filePath2);
+void testcase1();
+void testcase2();
+void testcase3();
+void manualResults(bool answer);
+void displayComparison(string file1, string file2);
 
-int main()
+string prompt1(string file1)
+{	
+	cout << "Enter the first file path: ";
+	cin >> file1;
+
+	return file1;
+}
+
+string prompt2(string file2)
 {
-	string file1 = "/home/user/secret/password.txt";
-	string file2 = "./../secret/password.txt";
-/*
-	cout << "Specify the first filename: ";
-	cin >> (file1);
-
-	cout << "Specify the second filename: ";
+	cout << "Enter the second file path: ";
 	cin >> file2;
-*/
-	if (homograph(file1, file2))
-		cout << "The filepaths are homographs.\n";
-	else
-		cout << "The filepaths are non-Homographs.\n";
 
-	return 0;
+	return file2;
 }
 
-bool homograph(string file1, string file2) {
-	
-	vector<string> filePath1 = stringToVector(file1);
-	vector<string> filePath2 = stringToVector(file2);
-	
-	file1 = canon(filePath1);
-	file2 = canon(filePath2);
 
-	if (file1.compare(file2) == 0) {
-		return true;
-	}
-	else {
-		return false;
-	}
-}
+vector<string> stringConverter(string fileName)
+{
+	vector<string> outputVector;
+	stringstream stream(fileName);
+	string parsed;
 
-string canon(vector<string> filePath) {
-	stack<string> currentFileString;
-	
-	for (vector<string>::iterator iter = filePath.begin(); iter != filePath.end(); iter++)
+	if (fileName.find(":") != std::string::npos)
 	{
-		if (iter == filePath.begin())
-		{
-			bool foundColon = (*iter).find(":") != std::string::npos;
 
-			// if we are at the beginning of a path / - for unix C: - for windows - we don't want a current working directory.
-			if (*iter == "/" || foundColon)
+		while (getline(stream, parsed, '\\'))
+		{
+			if (parsed == "" || parsed == " ")
+				continue;
+
+			outputVector.push_back(parsed);
+		}
+	}
+	else
+	{
+		while (getline(stream, parsed, '/'))
+		{
+			if (parsed == "" || parsed == " ")
+				continue;
+
+			outputVector.push_back(parsed);
+		}
+	}
+
+	return outputVector;
+}
+
+string canonicalizer(vector<string> filePath)
+{
+	vector<string> vectFilePath = filePath;
+	map<int, string> filekey;
+	int count = 0;
+	string fileString = " ";
+	for (vector<string>::iterator iter = vectFilePath.begin(); iter != vectFilePath.end(); iter++)
+	{
+		count++;
+
+		if (iter == vectFilePath.begin())
+		{
+			bool colon = (*iter).find(":") != std::string::npos;
+
+			if (*iter == "/" || colon)
 			{
-				currentFileString = stack<string>();
+				filekey = map<int, string>();
 			}
 		}
 
-		if (*iter == "..")
+		if (*iter == "..") 
 		{
-			currentFileString.pop();
+			filekey.erase(count);
 		}
-		else if (*iter == ".")
+		else if (*iter == ".") 
 		{
 			continue;
 		}
@@ -79,81 +103,196 @@ string canon(vector<string> filePath) {
 		}
 		else
 		{
-			currentFileString.push(*iter);
+			filekey.emplace(count, *iter);      
 		}
-
 	}
+
+	map<int, string>::iterator it = filekey.begin();
+
+	while (it != filekey.end())
+	{
+		fileString = it->second;
+
+		it++;
+	}
+
+	return fileString;
+}
+
+bool homograph(string filePath1, string filePath2)
+{
+	vector<string> fileVector1 = stringConverter(filePath1);
+
+	vector<string> fileVector2 = stringConverter(filePath2);
+
+	string fileCanon1 = canonicalizer(fileVector1);
+	string fileCanon2 = canonicalizer(fileVector2);
+
+
+	if (fileCanon1.compare(fileCanon2) == 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void allPathsCheck(string filePath1, string filePath2)
+{
+	bool answer = homograph(filePath1, filePath2);
+	manualResults(answer);
+	cout << endl;
+	testcase1();
+	cout << endl;
+	testcase2();
+	cout << endl;
+	testcase3();
+	cout << endl;
+}
+
+void testcase1() //non-homographs
+{
+	string filepath1 = " /home/user/cse453/";
+	string filepath2 = "../user/../password.txt";
+
+	displayComparison(filepath1, filepath2);
+	bool answer = homograph(filepath1, filepath2);
+
+	manualResults(answer);
+
+}
+
+void testcase2() //non-homographs
+{
+	string filepath1 = "/home/user/cse453/";
+	string filepath2 = " /../../secret/password.txt";
+
+	displayComparison(filepath1, filepath2);
+	bool answer = homograph(filepath1, filepath2);
+
+	manualResults(answer);
+
+}
+
+void testcase3() //homographs
+{	
+
+	string filepath1 = "./../home/test.cpp";
+	string filepath2 = "/root/desktop/home/test.cpp";
+
+	displayComparison(filepath1, filepath2);
+	bool answer = homograph(filepath1, filepath2);
+
+	manualResults(answer);
+
+}
+
+void testcase4() //homographs
+{
+	string filepath1 = "/home/user/secret/password.txt";
+	string filepath2 = " ./../secret/password.txt";
+
+	displayComparison(filepath1, filepath2);
+	bool answer = homograph(filepath1, filepath2);
 	
-	stack<string> reversedStack = reverseStack(currentFileString);
-	string filePathName = "";
+	manualResults(answer);
 
-	while (!reversedStack.empty())
+}
+
+void manualResults(bool answer)
+{
+	if (answer == true)
 	{
-		if (reversedStack.top() == "/")
-		{
-			reversedStack.pop();
+		cout << "These paths are homographs.\n";
+	}
+	else
+	{
+		cout << "These paths are not homographs.\n";
+	}
+}
+
+void displayComparison(string file1, string file2)
+{
+	cout << "The following file paths are being tested for homograph comparison:\n";
+	cout << "\t First filepath: " << file1 << endl;
+	cout << "\t Second filepath: " << file2 << endl;
+
+}
+
+int main() 
+{
+	string file1;
+	string file2;
+	char userInput[2];
+	bool answer = true;
+
+	cout << "Menu Options\n";
+	cout << "\t M (Manual Input)\n";
+	cout << "\t N (Testcase 1)\n";
+	cout << "\t O (Testcase 2)\n";
+	cout << "\t P (Testcase 3)\n";
+	cout << "\t R (Testcase 4)\n";
+	cout << "\t A (All paths check)\n";
+	cout << "\t Q (Quit)\n";
+	cout << "Input: ";
+	do {
+		cin.getline(userInput, 2);
+		
+		if (cin.fail()) {
+			cin.clear();
+			cin.ignore();
 			continue;
-		}
+			}
 
-		if (filePathName == "")
-		{
-			filePathName += reversedStack.top();
-		}
-		else
-		{
-			filePathName = filePathName + "/" + reversedStack.top();
-		}
-
-		reversedStack.pop();
-	}
-	
-	return filePathName;
-
-}
-
-stack<string> reverseStack(stack<string> stringStack)
-{
-	stack<string> reversedStack;
-
-	while (!stringStack.empty())
-	{
-		reversedStack.push(stringStack.top());
-		stringStack.pop();
-	}
-
-	return reversedStack;
-}
-
-vector<string> stringToVector(string file)
-{
-	vector<string> newVector;
-	stringstream str_strm(file);
-	string parsed;
-
-	while (getline(str_strm, parsed, '/'))
-	{
-		if (parsed == "" || parsed == " ")
+		if (isalpha(userInput[0]))
+			userInput[0] = toupper(userInput[0]);
+		
+		if (isdigit(userInput[0]))
 			continue;
 
-		newVector.push_back(parsed);
-	}
+		switch (userInput[0])
+		{
+		case 'M':
+			file1 = prompt1(file1);
+			file2 = prompt2(file2);
+			answer = homograph(file1, file2);
+			manualResults(answer);
+			break;
+		case 'N':
+			testcase1();
+			break;
+		case 'O':
+			testcase2();
+			break;
+		case 'P':
+			testcase3();
+			break;
+		case 'R':
+			testcase4();
+			break;
+		case 'A':
+			file1 = prompt1(file1);
+			file2 = prompt2(file2);
+			allPathsCheck(file1, file2);
+			break;
+		case 'Q':
+			break;
+		default:
+			cout << "Menu Options\n";
+			cout << "\t M (Manual Input)\n";
+			cout << "\t N (Testcase 1)\n";
+			cout << "\t O (Testcase 2)\n";
+			cout << "\t P (Testcase 3)\n";
+			cout << "\t R (Testcase 4)\n";
+			cout << "\t A (All paths check)\n";
+			cout << "\t Q (Quit)\n";
+			cout << "Input: ";
+			break;
+		}
 
+	} while (userInput[0] != 'q' && userInput[0] != 'Q');
 
-	return newVector;
-}
-
-string getCurrentPath(stack<string>& pathVector)
-{
-	// this uses the std::filesystem library to get the current working directory path
-	path currentPath = current_path();
-	path::iterator currentPathIter;
-
-	string currentPathString = currentPath.generic_string();
-
-	for (currentPathIter = currentPath.begin(); currentPathIter != currentPath.end(); currentPathIter++)
-	{
-		pathVector.push((*currentPathIter).generic_string());
-	}
-
-	return currentPathString;
+	return 0;
 }
